8b807f34499990c1fd384213b852a150bca17c0e
Johannes van Woerkom <johannesvw3@gmail.com>
2021-07-25 12:31:36 -0400

    fixed bug with media query width change

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+const mediaQueryMobile = window.matchMedia('(max-width: 600px)');
+const mediaQueryNormal = window.matchMedia('(min-width: 601px)');
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "150px";
 }
@@ -6,16 +9,39 @@ function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
 }
 
+function touchMenu() {
 window.addEventListener('touchstart', function(event){
 	let menu = document.getElementById('mySidebar');
 	if (event.target != menu && event.target.parentNode != menu){
         menu.style.width = '0';
     }
 });
+}
 
+function mouseTouch() {
 window.addEventListener('mouseup', function(event){
 	let menu = document.getElementById('mySidebar');
 	if (event.target != menu && event.target.parentNode != menu){
         menu.style.width = '0';
     }
 });
+}
+
+
+//Media Query Width Change handler
+  if (matchMedia) {
+    const mq = window.matchMedia("(min-width: 601px)");
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+    }
+    
+    // media query change
+    function WidthChange(mq) {
+    if (mq.matches) {
+      document.getElementById('mySidebar').style.justifyContent = 'center'
+    } else {
+      mouseTouch();
+      touchMenu();
+      document.getElementById('mySidebar').style.justifyContent = 'start'
+    }
+    }
