508a434aafe14b5c4fade0017bc003f7ad1d0fed
Johannes van Woerkom <johannesvw33@gmail.com>
2023-01-19 02:12:03 -0500

    New Website

---
+++ avw/src/components/menu/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+
+const CTA = () => {
+  return (
+    <div className='cta_sidenav'>
+        
+    </div>
+  )
+}
+
+export default CTA
